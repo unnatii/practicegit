184e8bad94e398161c15622ffec695dff857ac82
unnatii <unnatitibrewal@gmail.com>
2017-02-07 00:58:09 +0530

    2nd file

---
+++ stack.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#define size 25
+ struct stack
+{
+ int a[size];
+ int top;
+}s;
+
+
+ int eval(int a,int b,char c);
+void push(int x)
+{
+ if((s.top)==size-1)
+ {
+   printf("stack overflow");
+ }
+  else 
+  {
+   (s.a[s.top++])=x;
+  }
+}
+   
+
+int pop()
+{
+  if((s.top)==-1)
+  {
+    printf("empty stack");return -1;
+  }
+  else 
+  {
+    return (s.a[s.top--]);
+  }
+}
+
+void main()
+{s.top=-1;
+  
+  char c[size];int e;
+  printf("enter postfix expression");
+   scanf("%s",c);
+   e=evalpostfix(c);
+  printf("postfix evaluation=%d\n",e);
+}
+  
+
+int evalpostfix(char c[])
+{ 
+   int ans,i=0,op1,op2;
+   char ch;
+   while((ch=c[i])!='\0')
+    {
+       if((ch>='0')&&(ch<='9'))
+        push(ch-'0');
+       else
+       {
+         op2=pop();
+         op1=pop();
+         ans=eval(op1,op2,ch);
+         push(ans);
+       }i++;
+    }
+    return ans;
+}
+  
+ int eval(int a,int b,char c)
+ {
+   switch(c)
+   {
+     case '+':return (a+b);
+     case '-':return (a-b);
+     case '/':return (a/b);
+     case '*':return (a*b);
+    }
+  }
